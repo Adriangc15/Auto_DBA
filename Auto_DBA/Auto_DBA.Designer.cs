19a1d2675d85d3e1d59a868978c23ee90606777d
Chavarria Alvarez, Adrian G <adrian.g.chavarria.alvarez@intel.com>
2018-10-25 19:17:13 -0600

    Agregando el memu de opciones

---
+++ Auto_DBA/Auto_DBA.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Auto_DBA
+{
+    partial class Auto_DBA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.respaldosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recuperarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.administracionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablespaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.archivosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.respaldosToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.directoriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.esquemasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.auditoriasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.monitoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tunningDeConsultasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rendimientoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.respaldosToolStripMenuItem,
+            this.administracionToolStripMenuItem,
+            this.auditoriasToolStripMenuItem,
+            this.monitoreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1301, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // respaldosToolStripMenuItem
+            // 
+            this.respaldosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crearToolStripMenuItem,
+            this.recuperarToolStripMenuItem});
+            this.respaldosToolStripMenuItem.Name = "respaldosToolStripMenuItem";
+            this.respaldosToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.respaldosToolStripMenuItem.Text = "Respaldos";
+            // 
+            // crearToolStripMenuItem
+            // 
+            this.crearToolStripMenuItem.Name = "crearToolStripMenuItem";
+            this.crearToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.crearToolStripMenuItem.Text = "Crear";
+            // 
+            // recuperarToolStripMenuItem
+            // 
+            this.recuperarToolStripMenuItem.Name = "recuperarToolStripMenuItem";
+            this.recuperarToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.recuperarToolStripMenuItem.Text = "Recuperar";
+            // 
+            // administracionToolStripMenuItem
+            // 
+            this.administracionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tablespaceToolStripMenuItem,
+            this.archivosToolStripMenuItem,
+            this.respaldosToolStripMenuItem1,
+            this.directoriosToolStripMenuItem,
+            this.esquemasToolStripMenuItem});
+            this.administracionToolStripMenuItem.Name = "administracionToolStripMenuItem";
+            this.administracionToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.administracionToolStripMenuItem.Text = "Administracion";
+            // 
+            // tablespaceToolStripMenuItem
+            // 
+            this.tablespaceToolStripMenuItem.Name = "tablespaceToolStripMenuItem";
+            this.tablespaceToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.tablespaceToolStripMenuItem.Text = "Tablespace";
+            // 
+            // archivosToolStripMenuItem
+            // 
+            this.archivosToolStripMenuItem.Name = "archivosToolStripMenuItem";
+            this.archivosToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.archivosToolStripMenuItem.Text = "Archivos";
+            // 
+            // respaldosToolStripMenuItem1
+            // 
+            this.respaldosToolStripMenuItem1.Name = "respaldosToolStripMenuItem1";
+            this.respaldosToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.respaldosToolStripMenuItem1.Text = "Respaldos";
+            // 
+            // directoriosToolStripMenuItem
+            // 
+            this.directoriosToolStripMenuItem.Name = "directoriosToolStripMenuItem";
+            this.directoriosToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.directoriosToolStripMenuItem.Text = "Directorios";
+            // 
+            // esquemasToolStripMenuItem
+            // 
+            this.esquemasToolStripMenuItem.Name = "esquemasToolStripMenuItem";
+            this.esquemasToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.esquemasToolStripMenuItem.Text = "Esquemas";
+            // 
+            // auditoriasToolStripMenuItem
+            // 
+            this.auditoriasToolStripMenuItem.Name = "auditoriasToolStripMenuItem";
+            this.auditoriasToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.auditoriasToolStripMenuItem.Text = "Auditorias";
+            // 
+            // monitoreToolStripMenuItem
+            // 
+            this.monitoreToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tunningDeConsultasToolStripMenuItem,
+            this.rendimientoToolStripMenuItem});
+            this.monitoreToolStripMenuItem.Name = "monitoreToolStripMenuItem";
+            this.monitoreToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.monitoreToolStripMenuItem.Text = "Monitoreo";
+            // 
+            // tunningDeConsultasToolStripMenuItem
+            // 
+            this.tunningDeConsultasToolStripMenuItem.Name = "tunningDeConsultasToolStripMenuItem";
+            this.tunningDeConsultasToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.tunningDeConsultasToolStripMenuItem.Text = "Tunning de Consultas";
+            // 
+            // rendimientoToolStripMenuItem
+            // 
+            this.rendimientoToolStripMenuItem.Name = "rendimientoToolStripMenuItem";
+            this.rendimientoToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.rendimientoToolStripMenuItem.Text = "Rendimiento";
+            // 
+            // Auto_DBA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1301, 622);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Auto_DBA";
+            this.Text = "Auto DBA Oracle";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem respaldosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recuperarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem administracionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablespaceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem archivosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem respaldosToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem directoriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem esquemasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem auditoriasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem monitoreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tunningDeConsultasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rendimientoToolStripMenuItem;
+    }
+}
+
